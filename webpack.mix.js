7dd9cb0aa18403b67075957e3918e589318b676e
Viktor Bezdenezhnykh <bv@leber.ru>
2024-09-29 11:26:29 +0500

    INT-1 webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('dotenv').config()
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +7,45 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+const glob = require('glob')
+
+/*
+ |--------------------------------------------------------------------------
+ | Vendor assets
+ |--------------------------------------------------------------------------
+ */
+
+function mixAssetsDir(query, cb) {
+    ;(glob.sync('resources/' + query) || []).forEach(f => {
+        f = f.replace(/[\\\/]+/g, '/')
+        cb(f, f.replace('resources', 'public'))
+    })
+}
+
+const sassOptions = {
+    precision: 5,
+    includePaths: ['node_modules', 'resources/assets/']
+}
+
+mixAssetsDir('scss/**/!(_)*.scss', (src, dest) =>
+    mix.sass(src, dest.replace(/(\\|\/)scss(\\|\/)/, '$1css$2').replace(/\.scss$/, '.css'), {sassOptions})
+)
+
+// vendors
+mixAssetsDir('vendors/js/**/*.js', (src, dest) => mix.scripts(src, dest))
+mixAssetsDir('vendors/css/**/*.css', (src, dest) => mix.copy(src, dest))
+
+// script js
+mixAssetsDir('js/pages/**/*.js', (src, dest) => mix.scripts(src, dest))
+
+// plugins js
+mixAssetsDir('js/plugins/**/*.js', (src, dest) => mix.scripts(src, dest))
+
+mix.copyDirectory('resources/images', 'public/images')
+
+mix.js(['resources/js/app.js', 'resources/js/bootstrap.js'], 'public/js')
