cb7386fb707ad4c206e033e704d62fbe71936db8
Viktor Bezdenezhnykh <bv@leber.ru>
2024-09-29 23:05:14 +0500

    INT-1 more assets added & service providers, sanctum, cors, kernel setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\View\Components\Button;
+use App\View\Components\Icon;
+use App\View\Components\Input;
+use App\View\Components\Label;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('input', Input::class);
+        Blade::component('label', Label::class);
+        Blade::component('icon', Icon::class);
+        Blade::component('button', Button::class);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+global.feather = require('feather-icons');
+global.$ = global.jQuery = require('jquery')
+import DataTable from "datatables.net-dt";
+import 'datatables.net-buttons-dt';
+import 'datatables.net-fixedheader-dt';
+import 'datatables.net-select-dt';
+window.DataTable = DataTable;
+
+require('./base/sidebar');
